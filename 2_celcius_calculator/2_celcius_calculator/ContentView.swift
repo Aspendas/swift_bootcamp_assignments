fb037ca31b6f6b61415741d9e648f86ca4731c13
Aspendas <umutyasin2001@gmail.com>
2024-08-04 19:28:57 +0300

    add celsius to fahrenheit calculation

---
+++ 2_celcius_calculator/2_celcius_calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2_celcius_calculator
+//
+//  Created by Umut Y. Çalışkan on 3.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2_celcius_calculator/2_celcius_calculator/__celcius_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  __celcius_calculatorApp.swift
+//  2_celcius_calculator
+//
+//  Created by Umut Y. Çalışkan on 3.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct __celcius_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
